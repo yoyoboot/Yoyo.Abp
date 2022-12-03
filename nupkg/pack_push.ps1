fb1a974a514dbe14398d8be36df8222ffa181df9
stan e <yi.hang@live.com>
2022-12-03 21:19:33 +0800

    修改打包推送

--- nupkg/pack_push.ps1
+++ nupkg/pack_push.ps1
@@ -1,16 +1,24 @@
 $source = $env:NUGET_SOURCE
 $apikey = $env:NUGET_SOURCE_APIKEY
 $fileName = $env:PUSH_FILE_NAME
+$disableApiKey = $env:DISABLE_API_KEY
+$version = $env:TAG
+
 
 # $source = 'http://nuget.org/index.json'
 # $apikey = 'key'
-# $fileName = './aaa.ps1'
+# $fileName = './a-push.ps1'
+# $version = '7.3.0.1'
 
 $prefix = 'dotnet nuget push '
 $suffix = ' --source "' + $source + '" --api-key "' + $apikey + '" --skip-duplicate'
+if ($disableApiKey -eq $True) {
+    $suffix = ' --source "' + $source + '" --skip-duplicate'
+}
 
 # 生成脚本
-Get-ChildItem ./*.nupkg | Select-Object { $prefix + $_.Name + $suffix }  `
+Get-ChildItem ./*.nupkg | Where-Object { $_.Name -match $version } `
+| Select-Object { $prefix + $_.Name + $suffix }  `
 | Out-File -width 1000 $fileName -Force
 
 (Get-Content $fileName | Select-Object -Skip 3) | Set-Content $fileName
