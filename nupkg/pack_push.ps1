c608220a10e45605e406b8f66914c9ff33f63281
stan e <yi.hang@live.com>
2022-12-03 21:34:56 +0800

    移除冗余的脚本

--- nupkg/pack_push.ps1
+++ nupkg/pack_push.ps1
@@ -2,13 +2,10 @@ $source = $env:NUGET_SOURCE
 $apikey = $env:NUGET_SOURCE_APIKEY
 $fileName = $env:PUSH_FILE_NAME
 $disableApiKey = $env:DISABLE_API_KEY
-$version = $env:TAG
-
 
 # $source = 'http://nuget.org/index.json'
 # $apikey = 'key'
 # $fileName = './a-push.ps1'
-# $version = '7.3.0.1'
 
 $prefix = 'dotnet nuget push '
 $suffix = ' --source "' + $source + '" --api-key "' + $apikey + '" --skip-duplicate'
@@ -17,8 +14,7 @@ if ($disableApiKey -eq $True) {
 }
 
 # 生成脚本
-Get-ChildItem ./*.nupkg | Where-Object { $_.Name -match $version } `
-| Select-Object { $prefix + $_.Name + $suffix }  `
+Get-ChildItem ./*.nupkg | Select-Object { $prefix + $_.Name + $suffix }  `
 | Out-File -width 1000 $fileName -Force
 
 (Get-Content $fileName | Select-Object -Skip 3) | Set-Content $fileName
