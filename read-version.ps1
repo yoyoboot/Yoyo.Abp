dda9ebb5f54c170a04c5aed7c5bcf3cc01a22f7c
stan e <yi.hang@live.com>
2024-03-25 17:44:41 +0800

    全局配置参数更新

--- read-version.ps1
+++ read-version.ps1
@@ -1,7 +1,2 @@
-# 输出Hello World!
-Write-Host "Hello World!"
-# 获取当前日期和时间
-$now = Get-Date
-Write-Host "当前日期和时间：$now"
 [xml]$xml = Get-Content .\common.props
 Write-Host $xml.SelectSingleNode('//Version')."#text"
